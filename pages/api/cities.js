2d13e8f364c0a32d420404a6642f863915399c47
Nivando Soares <sidvandoni@gmail.com>
2022-07-04 09:37:48 -0300

    test get route

--- pages/api/cities.js
+++ pages/api/cities.js
@@ -1,8 +1,36 @@
 import {
-    cities
-  } from "../../lib/city.list.json";
-  
-  export default function handler(req, res) {
-    res.status(200)
-      .json(cities)
+  useState
+} from 'react'
+
+function EndpointsPage() {
+  const [cities, setCities] = useState([])
+
+  const fetchCityList = async () => {
+    const response = await fetch('/api/search')
+    const data = await response.json()
+    console.log(data)
+    setBooks(data)
   }
+
+  return(
+    <>
+    <div align="center">
+      <button  onClick={fetchCityList}>Get the ID cities list </button>
+     </div> {
+      cities.map(city => {
+        return (
+          <div align="center" key={city.id}>
+            {city.id}.<br/>
+            {"City Name: "}{city.name}.<br/>
+            {"State: "} {city.state}.<br/>
+            {"Country: "}{city.country} <br/>
+            <hr/>
+          </div>
+        )
+      })
+    }
+     </>
+  )
+}
+
+export default EndpointsPage;
