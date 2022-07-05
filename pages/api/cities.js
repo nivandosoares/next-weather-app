62abf1c44cb8771142a0139d0376e10e16510b69
Nivando <sidvandoni@gmail.com>
2022-07-05 05:54:49 -0300

    merge conflict

--- pages/api/cities.js
+++ pages/api/cities.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { useState } from "react";
 function Cities() {
@@ -32,41 +31,3 @@ function Cities() {
 }
 
 export default Cities;
-=======
-import {
-  useState
-} from 'react'
-
-function EndpointsPage() {
-  const [cities, setCities] = useState([])
-
-  const fetchCityList = async () => {
-    const response = await fetch('/api/search')
-    const data = await response.json()
-    console.log(data)
-    setCities(data)
-  }
-
-  return(
-    <>
-    <div align="center">
-      <button  onClick={fetchCityList}>Get the ID cities list </button>
-     </div> {
-      cities.map(city => {
-        return (
-          <div align="center" key={city.id}>
-            {city.id}.<br/>
-            {"City Name: "}{city.name}.<br/>
-            {"State: "} {city.state}.<br/>
-            {"Country: "}{city.country} <br/>
-            <hr/>
-          </div>
-        )
-      })
-    }
-     </>
-  )
-}
-
-export default EndpointsPage;
->>>>>>> 729a9699d435a23cac1d42d9e294812d15dc3d74
